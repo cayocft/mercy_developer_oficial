aee7c30894523220c720dd6f5ebf20f86f07e1b3
Edgardo cayo <cayo.edgardo@gmail.com>
2024-06-06 16:33:37 -0400

    Proyecto con todos los crud creado excepto el de descripción del servicio y contiene todos los textos traducidos y botones con bootstrap

---
+++ MercDevs_ej2/Program.cs
@@ -0,0 +1,35 @@
+using MercDevs_ej2.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+// coneccion a la bdd
+builder.Services.AddDbContext <MercydevsEjercicio2Context>(options => 
+options.UseMySql(builder.Configuration.GetConnectionString("connection"),
+Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.4.25-mariadb")));
+//end bdd
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
